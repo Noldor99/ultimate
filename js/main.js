18cb9d82f1b277b755536f1f48389e2f057e5efc
Noldor99 <qwerr19942017@gmail.com>
2024-06-12 12:43:23 +0300

    counter

--- js/main.js
+++ js/main.js
@@ -1,78 +1,92 @@
-// Custom scripts
-// Мобильное меню бургер
-const burger = document.querySelector(".burger")
-const menu = document.querySelector(".menu")
-const body = document.querySelector("body")
-function burgerMenu() {
-  burger.addEventListener("click", () => {
-    if (!menu.classList.contains("active")) {
-      menu.classList.add("active")
-      burger.classList.add("active")
-      body.classList.add("locked")
-    } else {
-      menu.classList.remove("active")
-      burger.classList.remove("active")
-      body.classList.remove("locked")
-    }
+/* =========== Burger menu ================*/
+const iconMenu = document.querySelector(".icon-menu")
+const navLink = document.querySelectorAll(".menu__link")
+const mainElement = document.querySelector("main")
+const footerElement = document.querySelector("footer")
+
+if (iconMenu) {
+  const menuBody = document.querySelector(".menu__body")
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock")
+    iconMenu.classList.toggle("_active")
+    menuBody.classList.toggle("_active")
   })
 
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 991.98) {
-      menu.classList.remove("active")
-      burger.classList.remove("active-burger")
-      body.classList.remove("locked")
-    }
+  navLink.forEach((link) => {
+    link.addEventListener("click", function () {
+      document.body.classList.remove("_lock")
+      iconMenu.classList.remove("_active")
+      menuBody.classList.remove("_active")
+    })
+  })
+  ;[mainElement, footerElement].forEach((element) => {
+    element.addEventListener("click", function () {
+      document.body.classList.remove("_lock")
+      iconMenu.classList.remove("_active")
+      menuBody.classList.remove("_active")
+    })
   })
 }
-burgerMenu()
+/* =========== SHOW HEADER BLUR AND BACKGROUND ================*/
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header")
+  var scrollPosition = window.scrollY
 
-// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
-function fixedNav() {
-  const nav = document.querySelector("nav")
-
-  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-  const breakpoint = 1
-  if (window.scrollY >= breakpoint) {
-    nav.classList.add("fixed__nav")
+  if (scrollPosition > 20) {
+    header.classList.add("scroll-active")
   } else {
-    nav.classList.remove("fixed__nav")
+    header.classList.remove("scroll-active")
+  }
+})
+/*=============== SCROLL SECTIONS===============*/
+function onMenuLinkClick(e) {
+  const menuLink = e.target
+  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    const gotoBlock = document.querySelector(menuLink.dataset.goto)
+    const gotoBlockValue =
+      gotoBlock.getBoundingClientRect().top +
+      window.scrollY -
+      document.querySelector("header").offsetHeight
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth",
+    })
+    e.preventDefault()
   }
 }
-window.addEventListener("scroll", fixedNav)
 
-//Прокрутка
-const menuLinks = document.querySelectorAll(".menu__item-link[data-goto]")
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]")
 if (menuLinks.length > 0) {
   menuLinks.forEach((menuLink) => {
     menuLink.addEventListener("click", onMenuLinkClick)
   })
-
-  function onMenuLinkClick(e) {
-    const menuLink = e.target
-    if (
-      menuLink.dataset.goto &&
-      document.querySelector(menuLink.dataset.goto)
-    ) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto)
-      const gotoBlockValue =
-        gotoBlock.getBoundingClientRect().top +
-        pageYOffset -
-        document.querySelector("header").offsetHeight
-
-      if (menu.classList.contains("active")) {
-        document.body.classList.remove("locked")
-        menu.classList.remove("active")
-        //menuBody.classList.remove("active");
-      }
-
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: "smooth",
-      })
-      e.preventDefault()
-    }
-  }
 }
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]")
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 110,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".menu__body a[data-goto*=" + sectionId + "]"
+      )
+    if (sectionClass) {
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionClass.classList.add("active-link")
+      } else {
+        sectionClass.classList.remove("active-link")
+      }
+    }
+  })
+}
+
+window.addEventListener("scroll", scrollActive)
 
 function accordion() {
   const items = document.querySelectorAll(".accordion__item-trigger")
@@ -97,3 +111,130 @@ function accordion() {
   })
 }
 accordion()
+
+/*=============== Scroll Animation ===============*/
+const animItems = document.querySelectorAll("._anim-items")
+
+if (animItems.length > 0) {
+  // Запуск анимации с помощью события
+  window.addEventListener("scroll", animOnScroll)
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index]
+      const animItemHeight = animItem.offsetHeight
+      const animItemOffset = offset(animItem).top
+      // Регулирует момент старта анимации
+      const animStart = 4
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart
+      }
+
+      if (
+        window.scrollY > animItemOffset - animItemPoint &&
+        window.scrollY < animItemOffset + animItemHeight
+      ) {
+        // Время задержки анимации
+        setTimeout(() => {
+          animItem.classList.add("_active")
+        }, 30)
+      } else {
+        // _anim-no-hide убирает анимацию появления по кругу c помощью CSS
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          // Повторяет анимацию по кругу /  если она не нужна, убираем animItem.classList.remove('_active');
+          // animItem.classList.remove('_active');
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+      scrollTop = window.scrollY || document.documentElement.scrollTop
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+  // Активирует функцию без действия скролла (Чтобы анимация сработала сразу после загрузке страницы)
+  animOnScroll()
+}
+
+/*=============== COUNTDOWN TIMER ===============*/
+document.addEventListener("DOMContentLoaded", () => {
+  const date = new Date("Apr 1 2024 00:00:00")
+
+  const daysVal = document.querySelector(".count-time__days .count-time__value")
+  const hoursVal = document.querySelector(
+    ".count-time__hours .count-time__value"
+  )
+  const minutesVal = document.querySelector(
+    ".count-time__minutes .count-time__value"
+  )
+  const secondsVal = document.querySelector(
+    ".count-time__seconds .count-time__value"
+  )
+
+  const daysText = document.querySelector(".count-time__days .count-time__text")
+  const hoursText = document.querySelector(
+    ".count-time__hours .count-time__text"
+  )
+  const minutesText = document.querySelector(
+    ".count-time__minutes .count-time__text"
+  )
+  const secondsText = document.querySelector(
+    ".count-time__seconds .count-time__text"
+  )
+
+  function declOfNum(number, titles) {
+    let cases = [2, 0, 1, 1, 1, 2]
+    return titles[
+      number % 100 > 4 && number % 100 < 20
+        ? 2
+        : cases[number % 10 < 5 ? number % 10 : 5]
+    ]
+  }
+
+  const timeCount = () => {
+    let now = new Date()
+    let leftUntil = date - now
+
+    if (leftUntil <= 0) {
+      date.setDate(date.getDate() + 2)
+      leftUntil = date - now
+    }
+
+    let days = Math.floor(leftUntil / 1000 / 60 / 60 / 24)
+    let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24
+    let minutes = Math.floor(leftUntil / 1000 / 60) % 60
+    let seconds = Math.floor(leftUntil / 1000) % 60
+
+    days = days < 10 ? "0" + days : days
+    hours = hours < 10 ? "0" + hours : hours
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+
+    daysVal.textContent = days
+    hoursVal.textContent = hours
+    minutesVal.textContent = minutes
+    secondsVal.textContent = seconds
+
+    daysText.textContent = declOfNum(days, ["Day", "Day", "Days"])
+    hoursText.textContent = declOfNum(hours, ["Hour", "Hour", "Clock"])
+    minutesText.textContent = declOfNum(minutes, [
+      "Minute",
+      "Minutes",
+      "Minutes",
+    ])
+    secondsText.textContent = declOfNum(seconds, [
+      "Second",
+      "Seconds",
+      "Seconds",
+    ])
+  }
+
+  const animate = () => {
+    timeCount()
+    requestAnimationFrame(animate)
+  }
+
+  animate()
+})
